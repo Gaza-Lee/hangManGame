80859ddbc033fecc7f2bf21fa039afd9678acee4
GazaLee <67987968+Gaza-Lee@users.noreply.github.com>
2025-06-04 15:12:57 +0000

    Add project files.

---
+++ hangManGame/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hangManGame
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ hangManGame/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace hangManGame
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
