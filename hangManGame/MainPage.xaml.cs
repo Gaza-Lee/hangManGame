372038a65a20e7bea1486d40e75a5f63136f972f
GazaLee <67987968+Gaza-Lee@users.noreply.github.com>
2025-06-20 03:25:04 +0000

    Enhance game UI and logic in MainPage
    Updated MainPage.xaml to improve layout with spacing and data binding for game status and messages. Removed static letter button and implemented dynamic letter buttons using `BindableLayout.ItemTemplate`.
    
    In MainPage.xaml.cs, added properties for managing game state, updated word list, and introduced methods for game logic, including win/loss checks and button interactions for guessing letters and resetting the game.

--- hangManGame/MainPage.xaml.cs
+++ hangManGame/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Data;
 using System.Diagnostics;
 
 namespace hangManGame
@@ -14,7 +15,45 @@ namespace hangManGame
                 spotlight = value;
                 OnPropertyChanged();
             }
+        }
 
+        public List<Char> Letters
+        {
+            get => letters;
+            set
+            {
+                letters = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Message
+        {
+            get => message;
+            set
+            {
+                message = value;
+                OnPropertyChanged();
+            }
+        }
+        public string GameStatus
+        {
+            get => gameStatus;
+            set
+            {
+                gameStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string CurrentImage
+        {
+            get => currentImage;
+            set
+            {
+                currentImage = value;
+                OnPropertyChanged();
+            }
         }
         #endregion
 
@@ -23,43 +62,47 @@ namespace hangManGame
         {
             "python",
             "javascript",
-            "csharp",
-            "java",
-            "ruby",
-            "swift",
-            "kotlin",
-            "typescript",
-            "golang",
-            "rust",
-            "php",
-            "html",
-            "css",
-            "sql",
-            "react",
             "maui",
-            "mogodb"
+            "csharp",
+            "mongodb",
+            "sql",
+            "xaml",
+            "word",
+            "excel",
+            "powerpoint",
+            "code",
+            "hotreload",
+            "snippets"
         };
 
         string answer = "";
         private string spotlight;
         List<char> guessed =
             new List<char>();
+        private List<char> letters = 
+            new List<char>();
+        private string message;
+        private int mistakes = 0;
+        private int maxMixtakes = 6;
+        private string gameStatus;
+        private string currentImage = "img0.jpg";
         #endregion
 
         public MainPage()
         {
             InitializeComponent();
+            Letters.AddRange("abcdefghijklmnopqrstuvwxyz");
             BindingContext = this;
             PickWord();
             CalculateWord(answer, guessed);
-            
+
         }
 
         #region Game Engine
         private void PickWord()
         {
             answer =
-                words[new Random().Next (0, words.Count)];
+                words[new Random().Next(0, words.Count)];
             Debug.WriteLine(answer);
         }
 
@@ -70,6 +113,96 @@ namespace hangManGame
                 .ToArray();
             Spotlight = string.Join(" ", temp);
         }
+        private void HandleGuess(char letter)
+        {
+            if (guessed.IndexOf(letter) == -1)
+            {
+                guessed.Add(letter);
+            }
+            if (answer.IndexOf(letter) >= 0)
+            {
+                CalculateWord(answer, guessed);
+                CheckIfGameWon();
+            }
+            else if (answer.IndexOf(letter) == -1)
+            {
+                mistakes++;
+                UpdateGameStatus();
+                CheckIfGameLost();
+                CurrentImage = $"img{mistakes}.jpg";
+            }
+        }
+
+        private void CheckIfGameLost()
+        {
+            if (mistakes == maxMixtakes)
+            {
+                Message = "You Lose";
+                DisableLetters();
+            }
+        }
+
+        private void DisableLetters()
+        {
+            foreach (var children in LettersContainer.Children)
+            {
+                var btn = children as Button;
+                if (btn != null)
+                {
+                    btn.IsEnabled = false;
+                }
+            }
+        }
+        private void EnableLetters()
+        {
+            foreach (var children in LettersContainer.Children)
+            {
+                var btn = children as Button;
+                if (btn != null)
+                {
+                    btn.IsEnabled = true;
+                }
+            }
+        }
+
+        private void CheckIfGameWon()
+        {
+            if (Spotlight.Replace(" ", "") == answer)
+            {
+                Message = "You Win";
+                DisableLetters();
+            }
+        }
+
+        private void UpdateGameStatus()
+        {
+            GameStatus = $"Errors: {mistakes} of {maxMixtakes}";
+        }
         #endregion
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                var letter = btn.Text;
+                btn.IsEnabled = false;
+                HandleGuess(letter[0]);
+            }
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            mistakes = 0;
+            guessed.Clear();
+            PickWord();
+            CalculateWord(answer, guessed);
+            Message = "Guess the word!";
+            GameStatus = $"Errors: {mistakes} of {maxMixtakes}";
+            CurrentImage = "img0.jpg";
+            UpdateGameStatus();
+            EnableLetters();
+        }
     }
 }
+
